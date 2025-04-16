317a8b382040015b520a563b7a4605d789ee830a
Tyner Sellers <twsellers@isengard.mines.edu>
2025-04-16 16:19:48 -0600

    fixed error

--- connectToDB.py
+++ connectToDB.py
@@ -1,10 +1,13 @@
 import pg8000
 from getpass import getpass
 
-connection = pg8000.connect(
-    user="UPDATE_ME",  # Replace with your username
-    password=getpass(),
-    host="ada.mines.edu",
-    port=5432,
-    database="csci403"
-)
+
+class Connection:
+    def __init__(self):
+        self.connection = pg8000.connect(
+            user="UPDATE_ME",  # Replace with your username
+            password=getpass(),
+            host="ada.mines.edu",
+            port=5432,
+            database="csci403"
+        )
